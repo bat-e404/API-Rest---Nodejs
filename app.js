78a7c368ff979b60b0a9732a389526b1431b801f
Bryan Aguirre Tudon <bryan_tudon@outlook.com>
2024-02-05 22:04:50 -0600

    API END

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const PORT = 8080;
+
+//Middlware use - convert the body into json
+app.use(express.json());
 
 // Endpoint
 app.get('/tshirt', (req, res) => {
@@ -9,6 +13,36 @@ app.get('/tshirt', (req, res) => {
         size: 'large'
     })
 
+});
+
+// Endpoint to route params
+/*
+app.post('/tshirt/:id', (req, res) => {
+    const { id } = req.params;
+    // this cannot be done, come in middleware
+    const { logo } = req.body;
+
+    if (!logo) {
+        res.status(418).send({ message: 'We need a logo!' });
+    }
+
+    res.send({
+        tshirt: `👕 with your ${logo} and ID of ${id}`,
+    });
+});
+*/
+
+app.post('/tshirt/:id', (req, res) => {
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if (!logo) {
+        res.status(418).send({ message: 'We need a logo!' });
+    }
+
+    res.send({
+        tshirt: `👕 with your ${logo} and ID of ${id}`,
+    });
 });
 
 app.listen(
